c38548e17d160717fb636b700fac1debc5a9cf40
Ismael250908 <ismaelbarquero2007@gmail.com>
2025-10-29 22:13:53 +0300

    Update Grafica.py

--- BackEnd/Grafica.py
+++ BackEnd/Grafica.py
@@ -2,4 +2,30 @@
 
 import matplotlib.pylot as plt
 
-def 
+def generar_grafico():
+    """
+    Esta funcion utiliza los datos del usuario para generar un grafico utilizando matplotlib
+    """
+    x_data = []
+    y_data = []
+
+    print("Digite los datos del grafico (si desea finalizar, digite stop):")
+
+    while True:
+        x = input("Enter the x-value (or 'stop' to finish): ")
+        if x.lower() == 'stop':
+            break
+        y = input("Enter the y-value: ")
+        x_data.append(float(x))
+        y_data.append(float(y))
+
+    plt.figure(figsize=(8, 6))
+    plt.plot(x_data, y_data)
+    plt.xlabel("X-axis")
+    plt.ylabel("Y-axis")
+    plt.title("User-generated Graph")
+    plt.grid(True)
+    plt.show()
+
+
+generar_grafico()
